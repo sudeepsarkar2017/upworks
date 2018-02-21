274e6da937e223b3f9074c1c0cc6ba091a850570
sudeep <sudeep.sarkar1989@gmail.com>
2018-02-21 18:57:57 +0530

    protractor

--- Rough/rough.js
+++ Rough/rough.js
@@ -4,8 +4,16 @@ describe('Name of the group', function() {
     it('test', function() {
         browser.driver.manage().window().maximize();
         browser.get('https://www.upwork.com/');
-        element(by.id('q')).sendKeys('test');
-        browser.sleep(3000)
+        element(by.id('q')).sendKeys('test',protractor.Key.ENTER);
+
+      element(by.className('m-xs-top m-md-bottom')).getText().then(function(text){
+            console.log(text);
+            // expect(text).toMatch('Test Freelancers');
+        });
+
+        // console.log(jobSearch),
+        // expect(element(by.id('oSearchContractorsHeader').getText())).toMatch('test')
+        browser.sleep(10000)
     })
         
 });
